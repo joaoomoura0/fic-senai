9e4ab5d3ba482d0ab98682e1d0ad0b2e0530ac49
joaoomoura0 <mourajoaovictor325@gmail.com>
2025-10-01 15:09:57 -0300

    first commit - estrutura do projeto

---
+++ FIC/src/main/java/com/ficsenai/FIC/FicApplication.java
@@ -0,0 +1,13 @@
+package com.ficsenai.FIC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicApplication.class, args);
+	}
+
+}
---
+++ FIC/src/test/java/com/ficsenai/FIC/FicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficsenai.FIC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
